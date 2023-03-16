1286c41d2e717b661f6308eb68157b2543243876
Ansh Shukla <ansh.shukla270501@gmail.com>
2023-03-16 20:21:33 +0530

    controller not initialized fixed.

--- lib/detailed_Screen.dart
+++ lib/detailed_Screen.dart
@@ -32,9 +32,9 @@ class _DetailedScreenState extends State<DetailedScreen> {
   String apikey;
   _DetailedScreenState(this.id,this.nameseries,this.thumb,this.apikey,this.accesstk);
 
-  late YoutubePlayerController controller;
+  var controller;
 
-  String linkTail = "";
+  String linkTail = "Default";
   String response = '';
   List<dynamic> castList =[];
 
@@ -62,16 +62,42 @@ class _DetailedScreenState extends State<DetailedScreen> {
             mute: false,
           )
       );
-
     });
 
+    print("1111111111 inside getyt");
+
   }
+
+  // Future controllerinitialize() async{
+  //   setState(() {
+  //   controller = YoutubePlayerController(
+  //       initialVideoId: linkTail,
+  //       flags: YoutubePlayerFlags(
+  //         hideThumbnail: true,
+  //         hideControls: false,
+  //         loop: false,
+  //         autoPlay: false,
+  //         mute: false,
+  //       )
+  //   );
+  //   });
+  // }
+
 
   @override
   void initState() {
+
+    print(linkTail + "      11111111  before getyt" );
+
     getYTtail();
+
+    print(linkTail + "     11111111  after getyt" );
+
+    //controllerinitialize();
+
     super.initState();
   }
+  
 
   void dispose(){
     controller.dispose();
@@ -86,70 +112,94 @@ class _DetailedScreenState extends State<DetailedScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(44, 44, 44, 1.0),
-        body: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // controller.value.isInitialized?
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
-                    child: Container(
+      child: linkTail!="Default"? YoutubePlayerBuilder(
+        player: YoutubePlayer(
+          controller: controller,
+          showVideoProgressIndicator: false,),
+        builder: (context , Widget ) {
+          return Scaffold(
+            backgroundColor: Color.fromRGBO(44, 44, 44, 1.0),
+            body: Container(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // controller.value.isInitialized?
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(bottomLeft: Radius
+                            .circular(20), bottomRight: Radius.circular(20)),
+                        child: Container(
 
-                      height: 300,
-                      width: double.infinity,
-                      child:
-                      YoutubePlayer(
-                        controller: controller,
-                        showVideoProgressIndicator: false,)
-                   ),
-                  ),
-                ),
+                            height: 300,
+                            width: double.infinity,
+                            child: Widget
+                        ),
+                      ),
+                    ),
 
-                SizedBox(height: 20),
-                Headline("",nameseries as String),
-                SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(thumb,style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500
-                  ),),
+                    SizedBox(height: 20),
+                    Headline("", nameseries as String),
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(thumb, style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500
+                      ),),
+                    ),
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Text("CAST", style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 3,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800
+                      ),),
+                    ),
+                    CastScroll(castList),
+                    //Text(castList.toString(),style: TextStyle(color: Colors.amber),)
+                  ],
                 ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Text("CAST" , style: TextStyle(
-                    color: Colors.red,
-                    letterSpacing: 3,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800
-                  ),),
-                ),
-                CastScroll(castList),
-                //Text(castList.toString(),style: TextStyle(color: Colors.amber),)
-              ],
+              ),
+            ),
+            // floatingActionButton: FloatingActionButton(
+            //   onPressed: (){
+            //     setState(() {
+            //       if(controller.value.isPlaying == true){
+            //         controller.pause();
+            //       }else{
+            //         controller.play();
+            //       }
+            //     });
+            //   },
+            //   child: Icon(controller.value.isPlaying? Icons.play_arrow :Icons.pause),
+            // ),
+          );
+          // : Scaffold(
+          //   backgroundColor: Colors.black,
+          //   body: Center(
+          //     child: Text("Loading" , style: TextStyle(
+          //       color: Colors.red
+          //     ),),
+          //   ),
+          // );}
+        }) : Scaffold(
+        backgroundColor: Colors.black,
+          body: Center(
+            child:
+              CircularProgressIndicator(
+                color: Colors.red,
+              )
+
+
+
             ),
           ),
-        ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: (){
-        //     setState(() {
-        //       if(controller.value.isPlaying == true){
-        //         controller.pause();
-        //       }else{
-        //         controller.play();
-        //       }
-        //     });
-        //   },
-        //   child: Icon(controller.value.isPlaying? Icons.play_arrow :Icons.pause),
-        // ),
-      ),
+
     );
   }
 }
